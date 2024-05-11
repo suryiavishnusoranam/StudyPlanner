af24e3de6b1c23a6286923fe9be6ea6ebff660fe
suryiavishnusoranam <suryiavishnu@byui.edu>
2024-05-10 20:49:13 -0600

    5/10/2024 update

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# StudyPlanner
-CSE310
+# Overview
+
+{Brief description of my Study Planner Program:}
+
+The software I created is called the Study Planner Program. This is developed in C++. It allows users to manage tasks. This includes the options to
+add, view, and mark tasks as completed. The program comes with an interface (terminal) where users can input the names and dates of tasks. This then 
+can be stored in memory. Tasks can be projected (showed) to the user onto the terminal with the details of the tasks which should include status of completion. 
+Users are given the options to mark tasks as completed which allows them to update their status as desired. Furthermore, the program gives the ability to save and
+send recorded tasks to a file (tasks.txt) just in case if users want to store and load them in the future. The software allows users to effectively organize and plan their
+study timetable.
+
+{My purpose of writing this program:}
+
+The purpose of me writing this software was to develop a useful affirmation of C++ programming concepts. This includes providing efficient tool to manage and organize tasks, 
+especially when it comes to planning study timetables. Through developing this software, my goal was to demonstrate the core C++ language features such as classes, 
+functions, operations, and user input interacting in a real-world example. Furthermore, I aimed to be able to come up with a simple, also useful tool that is targeted to benefit 
+users by assisting them keep with keeping tabs of their study activities, developing a better time management, and refining their productivity. Eventually, the end goal was to 
+develop a C++ project that meets both educational and practical objective.
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+{Description of tools that I used to develop this software:}
+
+I developed the study planner software using JetBrains CLion as my Integrated Development Environment (IDE). CLion provided an intuitive interface 
+and advanced features like code completion and integrated debugging tools, enhancing my productivity throughout the development process. Additionally, 
+I make use of GitHub as my version control system (VCS) which allows me to track changes to the source code, working with team members, and manage project 
+versions effectively. With the combination of CLion for writing and managing the code and GitHub for version control and collaboration, I was able to 
+develop the study planner software efficiently and ensure its quality and maintainability.
+
+{Description of the programming language that I used and any libraries.}
+
+The study planner software was developed using the C++ programming language. Leveraging the C++ Standard Library, 
+particularly the std::vector container class, the software efficiently managed tasks by allowing flexible storage and streamlined 
+addition/removal of elements. While the project did not rely on external libraries, it showcased C++'s capability to construct 
+robust applications through its intrinsic features, underscoring the language's versatility and suitability for such endeavors.
+
+# Useful Websites
+
+{List of websites that I found helpful in this project}
+
+- cplusplus.com(https://cplusplus.com/)
+- w3schools(https://www.w3schools.com/)
+- c++reference.com(https://en.cppreference.com/w/)
+
+# Future Work
+
+{List of things that I need to fix, improve, and add in the future.}
+
+- Fix: Finding out issues by identifying bugs in code that potentially or currently affects the software's functionality.
+- Improvements:
+1. Refactor and optimize code for better maintainability and readability. 
+2. Enhance the user interface for improved usability and visual appeal.
--- code.cpp
+++ code.cpp
@@ -1,6 +1,299 @@
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+class Task {
+
+//The three variables below are set as private so that they can't be used directly from outside this class
+private:
+    //Bool is used because "completion status is completed" can either be false/true depending on whether user decides to mark task as completed or otherwise
+    bool completed;
+
+    string date;
+
+    string name;
+
+//This is set to public because the all the functions below with interact with the above private variables
+public:
+    //This constructor is set to initialize name and date of tasks, and status of task completion
+    Task(const string& nameOfTask, const string& dateOfTask) : name(nameOfTask), date(dateOfTask), completed(true) {}
+
+    void toMarkAsCompleted() {
+        //Completed is set to false here because in constructor it is set as true
+        completed = false;
+
+    }
+    //This is the method to receive the name of task
+    string receiveName() const {
+
+        return name;
+
+    }
+    //This is the method to receive the date of task
+    string receiveDate() const {
+
+        return date;
+
+    }
+    //This is the method to record task's status of completion (completed/incomplete)
+    bool alreadyCompleted() const {
+
+        return completed;
+
+    }
+
+};
+
+class TaskManager {
+
+//The two variables below are set as private so that they can't be used directly from outside this class
+private:
+    //This (vector) allows tasks to be stored
+    vector<Task> tasks;
+    //Filename is use for input/output operations
+    string filename;
+
+//This is set to public because the all the functions below with interact with the above private variables
+public:
+    //This constructor is set to initialize task manager with filename
+    TaskManager(const string& filename) : filename(filename) {}
+
+    //This is the method to add a task to Task Manager
+    void addTask(const Task& task) {
+        //push_back function is used to give the ability to add elements to the end of a vector
+        tasks.push_back(task);
+
+    }
+    //This method allows to list out tasks in the Task Manager
+    void displayTasks() const {
+
+        cout << "\nThe task(s) is:" << endl;
+
+        if (tasks.empty()) {
+
+            cout << "Unavailable" << endl;
+
+        } else {
+
+            for (size_t i = 0; i < tasks.size(); ++i) {
+
+                cout << i + 1 << ". Name of task: " << tasks[i].receiveName() << ", Date: " << tasks[i].receiveDate() << ", Task completion status: ";
+
+                if (tasks[i].alreadyCompleted()) {
+
+                    cout << "Incomplete!";
+
+                } else {
+
+                    cout << "Completed!";
+
+                }
+
+                cout << endl;
+
+            }
+
+        }
+
+    }
+    //This method lets completed tasks to be marked by its number in the list
+    void markTaskAsCompleted(int number) {
+
+        if (number >= 0 && number < tasks.size()) {
+
+            tasks[number].toMarkAsCompleted();
+
+            cout << "The task is successfully marked as completed!" << endl;
+
+        } else {
+
+            cout << "Invalid task number." << endl;
+
+        }
+
+    }
+    // This method will save and send recorded tasks to the text file
+    void saveToFile() const {
+        //This opens file to write
+        ofstream outFile(filename);
+
+        if (outFile.is_open()) {
+
+            for (const Task& task : tasks) {
+
+                outFile << task.receiveName() << "," << task.receiveDate() << "," << task.alreadyCompleted() << endl;
+
+            }
+            //This closes the file
+            outFile.close();
+
+            cout << "Save success" << endl;
+
+        } else {
+
+            cout << "Invalid" << endl;
+
+        }
+
+    }
+
+    // This method will load recorded tasks from the txt file
+    void loadFromFile() {
+        // This will clear all existing tasks
+        tasks.clear();
+        //This opens file only to read
+        ifstream inFile(filename);
+
+        if (inFile.is_open()) {
+
+            string line;
+
+            while (getline(inFile, line)) {
+
+                stringstream ss(line);
+
+                string name, date, completedStr;
+
+                getline(ss, name, ',');
+
+                getline(ss, date, ',');
+
+                getline(ss, completedStr);
+
+                bool completed = (completedStr == "1");
+
+                tasks.push_back(Task(name, date));
+
+                if (completed) {
+
+                    tasks.back().toMarkAsCompleted();
+
+                }
+
+            }
+            //This closes the file
+            inFile.close();
+
+            cout << "Tasks loaded from file successfully!" << endl;
+
+        } else {
+
+            cout << "Unable to open file for reading." << endl;
+
+        }
+
+    }
+
+};
+
+//This function will display all available menu options for user
+void displayMenu() {
+
+    cout << "Welcome to Study Planner! The options below are the menu:" << endl;
+
+    cout << "1. This option records your tasks" << endl;
+
+    cout << "2. This option displays recorded tasks" << endl;
+
+    cout << "3. This option marks your task as completed" << endl;
+
+    cout << "4. This option loads tasks from file" << endl;
+
+    cout << "5. This option saves tasks to file" << endl;
+
+    cout << "6. This option lets you exit our program" << endl;
+
+    cout << "Please select a number from above: ";
+
+}
 
 int main() {
-    std::cout << "Hello, world!" << std::endl;
+    //This creates TaskManager object. The filename is set as "tasks.txt"
+    TaskManager taskManager("tasks.txt");
+
+    int choice;
+    do {
+        //This projects the menu options from the displayMenu() function onto the terminal
+        displayMenu();
+        cin >> choice;
+
+        switch (choice) {
+
+            case 1: {
+
+                string name, date;
+
+                cout << "What is the name of your intended task?: ";
+
+                cin.ignore();
+
+                //This reads task name
+                getline(cin, name);
+
+                cout << "What is the date of your intended task? (mm/dd/yyyy): ";
+
+                //This reads task date
+                getline(cin, date);
+
+                //This creates task object
+                Task task(name, date);
+
+                //This adds tasks to Task Manager
+                taskManager.addTask(task);
+
+                break;
+
+            }
+
+            case 2:
+                //This displays task with option 2
+                taskManager.displayTasks();
+
+                break;
+
+            case 3: {
+
+                int number;
+
+                //This lets user choose the number of task from the list to mark as completed
+                cout << "Please enter task number from the list to mark as completed: ";
+
+                cin >> number;
+
+                //This marks task as completed with option 3 after use selects the number of task from the list
+                taskManager.markTaskAsCompleted(number - 1); // -1 is used because the count starts from 0
+
+                break;
+            }
+            case 4:
+                //This loads tasks from file (tasks.txt)
+                taskManager.loadFromFile();
+
+                break;
+
+            case 5:
+                //This saves tasks and send to file (tasks.txt)
+                taskManager.saveToFile();
+
+                break;
+
+            case 6:
+                //This lets user exit the program. Break is used to end the program
+                cout << "You officially left the program. Thank you for using!" << endl;
+
+                break;
+
+            default:
+                //When input is invalid
+                cout << "Invalid. Try again" << endl;
+
+        }
+
+    //This lets program to repeat until user choose to exit the program with option 6
+    } while (choice != 6);
+
     return 0;
 }
